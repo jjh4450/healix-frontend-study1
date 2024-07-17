e3d9dccc7a8869f2002962a46a1ae00c381a6078
Kim-jaeyeon <jy020507@naver.com>
2024-07-17 10:36:41 +0900

    refactor
    파일 구조 수정해보기

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from '../../widgets/Header'
+import Home from '../../widgets/Home'
+import About from '../../widgets/About'
+import Price from '../../widgets/Price'
+import Footer from '../../widgets/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <About/>
+      <Price/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
